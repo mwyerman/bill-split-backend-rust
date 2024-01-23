21e334a8cd5da694bee23576a1b242625727974c
Max Wyerman <mwyerman@gmail.com>
2024-01-22 21:36:03 -0500

    Set up basic integration test (#4)

--- src/api/handlers/bill_handler.rs
+++ src/api/handlers/bill_handler.rs
@@ -7,24 +7,24 @@ use axum::{
 use std::thread;
 use uuid::Uuid;
 use crate::data::Data;
-use crate::config::data_config::DataConfig;
+use crate::config::Config;
 
-pub async fn get_bills_route(State(config): State<DataConfig>) -> impl IntoResponse {
+pub async fn get_bills_route(State(config): State<Config>) -> impl IntoResponse {
     thread::spawn(move || {
-        let res = config.provider.get_bills();
+        let res = config.data.provider.get_bills();
         Json(res)
     }).join().unwrap()
 }
 
 pub async fn get_bill_from_id(
     Path(id): Path<String>,
-    State(config): State<DataConfig>
+    State(config): State<Config>
 ) -> impl IntoResponse {
     thread::spawn(move || {
         let uuid = Uuid::parse_str(&id);
         match uuid {
             Ok(uuid) => {
-                let res = config.provider.get_bill(uuid);
+                let res = config.data.provider.get_bill(uuid);
                 match res {
                     Some(bill) => (StatusCode::OK, Json(bill)).into_response(),
                     None => (StatusCode::NOT_FOUND, Json("Bill not found")).into_response()
